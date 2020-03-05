084129123908e7cda940e15daed03b6fb3fd68fa
grandhawa725 <grandhawa725@gmail.com>
2020-03-05 14:32:49 +1300

    Add project files.

---
+++ Band_Song_Database_MVC/Data/Migrations/20200225231824_Album_Song.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Band_Song_Database_MVC.Data.Migrations
+{
+    public partial class Album_Song : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
